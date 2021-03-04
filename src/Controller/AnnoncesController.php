137ed0b9637cbd16a20ea01095c77bd58513f878
Léo <demetleo@hotmail.fr>
2021-03-04 14:51:54 +0100

    1st commit

---
+++ src/Controller/AnnoncesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AnnoncesController extends AbstractController
+{
+    /**
+     * @Route("/annonces", name="annonces")
+     */
+    public function index(): Response
+    {
+        return $this->render('annonces/index.html.twig', [
+            'controller_name' => 'AnnoncesController',
+        ]);
+    }
+}
